eb42fdebb707e58a5288ed209d45bfd93f2f0b82
George Apetrei <george.apetrei@gmail.com>
2019-12-30 01:11:25 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,13 +1,16 @@
 def welcome
   # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
   # code #deal_card here
+  rand(1..11)
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(card)
+#puts "Your cards add up to 12"
+puts  "Your cards add up to #{card}"
 end
 
 def prompt_user
